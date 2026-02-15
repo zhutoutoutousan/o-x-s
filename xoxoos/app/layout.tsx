bb6b917f32d4d3733aa3369a6096403f81100218
zhutoutoutousan <mxyuiiu64545@126.com>
2026-02-15 19:33:03 +0100

    Add aggressive iOS visibility fixes for navigation menu

--- xoxoos/app/layout.tsx
+++ xoxoos/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
